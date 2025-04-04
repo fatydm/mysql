c1ac7e1ce22e8216523268ed757dc9ff59fb055b
faty <145115890+fatoumatad223@users.noreply.github.com>
2025-04-04 17:48:55 +0200

    fin du niveau 1

--- index.sql
+++ index.sql
@@ -1,4 +1,4 @@
--- Étape 1
+-- Étape 1 Création des tables 
 CREATE TABLE edusign (
 id SERIAL PRIMARY KEY,
 signedAt DATE
@@ -11,11 +11,31 @@ lastname TEXT NOT NULL,
 email VARCHAR(100) NOT NULL UNIQUE
 );
 
--- Étape 2
+-- Étape 2 Insertion des données 
 INSERT INTO users (firstname, lastname, email) VALUES 
 ('Faty', 'Diarra', 'fd@test.fr'),
 ('Aissata', 'Maiga', 'aima@test.fr'),
 ('Beatrice', 'Worsley', 'bea@test.fr'),
 ('Bella', 'Guerin', 'bella@test.fr'),
 ('Barbara', 'Chase', 'barbara@test.fr');
+
+-- Étape 3 Requêtes
+1. Sélectionner toutes les lignes de la table users
+    SELECT *  FROM users
+2. Sélectionner uniquement la ligne où le prénom est FATY dans la table users
+    SELECT * FROM users
+    WHERE firstname = 'Faty'    
+3. Sélectionner les lignes dont la première lettre du prénom est un B dans la table users
+    SELECT * FROM users
+    WHERE firstname LIKE 'B%'
+4. Compter le nombre de ligne qu’il y a dans la table users
+    SELECT * FROM users
+    COUNT (users)
+5. Compter le nombre de ligne dont la première lettre du prénom est un B dans la table users
+    SELECT COUNT(*) FROM users 
+    WHERE firstname LIKE 'B%'
+6. Afficher uniquement la colonne contenant le prénom de la table users.
+    SELECT firstname FROM users
+
+
 
